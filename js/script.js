457728f198975ac896b68ca4223a0af26f247ddd
Šejla Čajdin <sejla.cajdin@gmail.com>
2020-02-02 19:25:09 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+$(".owl-carousel").owlCarousel({
+    items:5,
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
+    margin:40,
+    autoWidth:true
+});
+var sliders=document.getElementsByClassName("slider");
+
+for(var i=0;i<sliders.length;i++){
+    var element=sliders[i];
+    element.oninput=function(){
+        
+        this.style.background="linear-gradient(to right, #00def8 20%, #18b3fa "+this.value+"%, #f4fafa "+this.value+"%,#f4fafa 100%)";
+    }
+}
+
+
